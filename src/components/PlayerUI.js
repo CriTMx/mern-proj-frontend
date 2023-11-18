9026acf1644cb2009c131d7aecb101d8fe2baf74
CriTMx <savefailedmc@gmail.com>
2023-11-19 01:09:11 +0530

    update player ui layout

--- src/components/PlayerUI.js
+++ src/components/PlayerUI.js
@@ -65,30 +65,31 @@ function PlayerUI({ playButtonIcon, isPlaying, updateCurrentDuration, updateTota
 
   return (
     <Container fluid='true' className='player-container text-white ps-5 pe-5 pt-4 pb-4' id='playerUI'>
-      <Row>
-        <div className='col-md-4 col-6'>
+      <Row className='player-container-row'>
+        <div className='col-md-4 col-12 d-flex justify-content-center justify-content-md-start player-song-instance-container'>
           <PlayerSongInstace playerSongImg={thumbnail} playerSongTitle={`${title}`} playerSongSubtitle={`By ${artist}`} />
+          <Button className='music-favorite-button music-favorite-button-mobile' onClick={() => handleFavButtonClick()}>
+            <span className='material-symbols-outlined d-flex align-self-center music-utility-icon mt-0' id='musicFavHeartIcon'>favorite</span>
+          </Button>
         </div>
-        <div className='col-md-4 d-none d-md-block text-center music-control-container'>
+        <div className='col-md-4 col-12 text-center music-control-container'>
           <div>
             <Button className='music-control-btn me-4'><span className='material-symbols-outlined symbol-filled d-flex align-self-center music-control-icon' id='prevMusicIcon'>skip_previous</span></Button>
             <Button className='music-control-btn me-4' onClick={() => handlePlayButton()}><span className='material-symbols-outlined symbol-filled d-flex align-self-center music-control-icon' id='playMusicIcon'>{playButtonIcon}</span></Button>
             <Button className='music-control-btn'><span className='material-symbols-outlined symbol-filled d-flex align-self-center music-control-icon' id='nextMusicIcon'>skip_next</span></Button>
           </div>
-          <div className='mt-3 d-flex flex-row align-items-center gap-2'>
+          <div className='seeker-bar-container mt-3 d-flex flex-row align-items-center gap-2'>
             <div className='float-start song-timer' id='songDurationCurrent'>0:00</div>
             <input type='range' onChange={sliderChange} id='seeker-bar' value={progress}></input>
             <div className='float-end song-timer' id='songDurationTotal'>5:00</div>
-            <div className='seeker-bar-container'>
-            </div>
           </div>
         </div>
-        <div className='col-md-4 col-6 d-flex align-items-center justify-content-end text-center music-utility-container'>
+        <div className='col-md-4 d-none d-md-flex align-items-center justify-content-end text-center music-utility-container'>
           <Button className='music-favorite-button me-md-4 me-2' onClick={() => handleFavButtonClick()}><span className='material-symbols-outlined d-flex align-self-center music-utility-icon mt-0' id='musicFavHeartIcon'>favorite</span></Button>
           <span className="material-symbols-outlined symbol-filled me-2 mt-0" id='musicVolumeIcon'>volume_up</span>
           <input type='range' id='volumeBar' onChange={() => handleVolumeBarChange()} min={0} max={100} value={volume} />
-          <Button className='expand-player-ui-btn' id='displayButton' onClick={() => handlePlayerUIDisplay()}><span className='material-symbols-outlined p-0 m-0'>expand_more</span></Button>
         </div>
+        <Button className='expand-player-ui-btn' id='displayButton' onClick={() => handlePlayerUIDisplay()}><span className='material-symbols-outlined p-0 m-0'>expand_more</span></Button>
       </Row>
       {/* <Button onClick={() => fetchAndPlayNewSong()}>Load New Song</Button> */}
     </Container>
