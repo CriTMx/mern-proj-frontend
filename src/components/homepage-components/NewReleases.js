1a28a162d6c4ab4488ebe2a7915a868b671efb74
CriTMx <savefailedmc@gmail.com>
2023-11-20 01:05:41 +0530

    update homepage

--- src/components/homepage-components/NewReleases.js
+++ src/components/homepage-components/NewReleases.js
@@ -2,16 +2,22 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import NewReleases_Instance from './NewReleases_Instance'
 import '../css/new-releases.css';
+import Skrillex from '../media/skrillex_logo.png'
+import SullivanKing from '../media/sullivanking_logo.png'
+import Zomboy from '../media/zomboy_logo.png'
+import MegAndDia from '../media/megdia_logo.png'
+import BMTH from '../media/bmth_logo.png'
+import BMTH2 from '../media/bmth_logo2.png'
 
 function NewReleases() {
   return (
     <Container fluid='true' className='text-white mb-5'>
         <h3 className='new-releases-title mb-4'>New Releases</h3>
         <div className='d-flex flex-wrap'>
-            <NewReleases_Instance newReleasesImg={''} newReleasesTitle={'New Release Placeholder'} newReleasesSubtitle={'By test'} />
-            <NewReleases_Instance newReleasesImg={''} newReleasesTitle={'New Release Placeholder'} newReleasesSubtitle={'By test'} />
-            <NewReleases_Instance newReleasesImg={''} newReleasesTitle={'New Release Placeholder'} newReleasesSubtitle={'By test'} />
-            <NewReleases_Instance newReleasesImg={''} newReleasesTitle={'New Release Placeholder'} newReleasesSubtitle={'By test'} />
+            <NewReleases_Instance newReleasesImg={Skrillex} newReleasesTitle={'Bangarang'} newReleasesSubtitle={'Skrillex'} />
+            <NewReleases_Instance newReleasesImg={BMTH} newReleasesTitle={'DArkSide'} newReleasesSubtitle={'Bring Me The Horizon'} />
+            <NewReleases_Instance newReleasesImg={BMTH} newReleasesTitle={'AmEN! (ft. Lil Uzi Vert & Daryl Palumbo'} newReleasesSubtitle={'Bring Me The Horizon'} />
+            <NewReleases_Instance newReleasesImg={Zomboy} newReleasesTitle={'Battlefields'} newReleasesSubtitle={'Zomboy'} />
         </div>
     </Container>
   )
--- src/components/homepage-components/RecentlyPlayed.js
+++ src/components/homepage-components/RecentlyPlayed.js
@@ -2,16 +2,23 @@ import React from 'react'
 import { Container, Row } from 'react-bootstrap'
 import '../css/recently-played.css';
 import RecentlyPlayed_Instance from './RecentlyPlayed_Instance';
+import Skrillex from '../media/skrillex_logo.png'
+import Skrillex2 from '../media/skrillex2_logo.png'
+import SullivanKing from '../media/sullivanking_logo.png'
+import Zomboy from '../media/zomboy_logo.png'
+import MegAndDia from '../media/megdia_logo.png'
+import BMTH from '../media/bmth_logo.png'
+import BMTH2 from '../media/bmth_logo2.png'
 
 function RecentlyPlayed() {
   return (
     <Container fluid='true' className='text-white mb-5'>
         <h3 className='recently-played-title mb-4'>Recently played</h3>
         <div className='d-flex flex-wrap'>
-          <RecentlyPlayed_Instance recentlyPlayedImg={''} recentlyPlayedTitle={'Recently Played Placeholder'} recentlyPlayedSubtitle={'By test'} />
-          <RecentlyPlayed_Instance recentlyPlayedImg={''} recentlyPlayedTitle={'Recently Played Placeholder'} recentlyPlayedSubtitle={'By test'} />
-          <RecentlyPlayed_Instance recentlyPlayedImg={''} recentlyPlayedTitle={'Recently Played Placeholder'} recentlyPlayedSubtitle={'By test'} />
-          <RecentlyPlayed_Instance recentlyPlayedImg={''} recentlyPlayedTitle={'Recently Played Placeholder'} recentlyPlayedSubtitle={'By test'} />
+          <RecentlyPlayed_Instance recentlyPlayedImg={BMTH2} recentlyPlayedTitle={'Shadow Moses'} recentlyPlayedSubtitle={'Bring Me The Horizon'} />
+          <RecentlyPlayed_Instance recentlyPlayedImg={Zomboy} recentlyPlayedTitle={'Deadweight'} recentlyPlayedSubtitle={'Zomboy'} />
+          <RecentlyPlayed_Instance recentlyPlayedImg={SullivanKing} recentlyPlayedTitle={'Lie'} recentlyPlayedSubtitle={'Sullivan King'} />
+          <RecentlyPlayed_Instance recentlyPlayedImg={Skrillex2} recentlyPlayedTitle={'Ragga Bomb'} recentlyPlayedSubtitle={'Skrillex, Ragga Twins'} />
         </div>
     </Container>
   )
